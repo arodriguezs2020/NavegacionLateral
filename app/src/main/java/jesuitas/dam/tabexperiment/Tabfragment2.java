d87bfc6c4a579ff2c1cdbec1487a369c56ea5954
Alvaro Rodriguez <dam16.2020@gmail.com>
2020-11-26 13:41:32 +0100

    Initial commit

---
+++ app/src/main/java/jesuitas/dam/tabexperiment/Tabfragment2.java
@@ -0,0 +1,23 @@
+package jesuitas.dam.tabexperiment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tabfragment2 extends Fragment {
+
+    public Tabfragment2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_fragment2, container, false);
+    }
+}
